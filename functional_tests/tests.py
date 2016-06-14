920378f78d4b9fe0654f13367e427decd9b27cce
tychovk <tychovk@gmail.com>
2016-06-13 23:05:13 -0500

    Bootstrap just improved the layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait 
 # element = WebDriverWait(driver, secs).until(find)
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
